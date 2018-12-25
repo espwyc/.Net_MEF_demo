fdd771aeb16177344809ff3a952984485500103e
richard <q314941534@gmail.com>
2018-12-25 15:23:59 +0800

    添加项目文件。

---
+++ MefTest/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MefTest
+{
+
+    //public interface IMyplugin
+    //{
+    //    string HelloWorld();
+    //    string Manifest();
+    //}
+    class Program
+    {
+
+        private CompositionContainer _container;
+        //
+        //MyPlugin namespace 
+        //
+        [Import(typeof(MyPlugin.IMyplugin))]
+        private MyPlugin.IMyplugin myplugin;
+
+
+        private Program()
+        {
+            TestPlugin();
+        }
+        private void TestPlugin()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog(@"plugins"));
+
+            _container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object
+            try
+            {
+                this._container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+            myplugin.Manifest();
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.myplugin.Manifest();
+
+            //myplugin.Manifest();
+
+            Console.WriteLine("Plugin Test Over");
+            Console.Write("请按任意键继续...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyPlugin/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace MyPlugin
+{
+    public interface IMyplugin
+    {
+        string HelloWorld();
+        string Manifest();
+    }
+    [Export(typeof(IMyplugin))]
+    public class MyPluginImpl : IMyplugin
+    {
+        public string HelloWorld()
+        {
+            return "hello world!";
+        }
+
+        public string Manifest()
+        {
+            string tmp = "hello, I am Plugin test";
+            Console.WriteLine(tmp);
+            return tmp;
+        }
+    }
+}
